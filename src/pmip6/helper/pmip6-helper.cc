8eeac5a1f6463e62bcbbe8ef67209498d5eb73fe
Hyon-Young Choi <commani@gmail.com>
2012-04-02 09:49:48 +0900

    Skeleton codes for PMIPv6

---
+++ src/pmip6/helper/pmip6-helper.cc
@@ -0,0 +1,11 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "pmip6-helper.h"
+
+namespace ns3 {
+
+/* ... */
+
+
+}
+
---
+++ src/pmip6/model/pmip6.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __PMIP6_H__
+#define __PMIP6_H__
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __PMIP6_H__ */
+
